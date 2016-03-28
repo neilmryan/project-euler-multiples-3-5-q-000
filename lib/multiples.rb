397da79825255f824736c568d040e34322afce5b
neilmryan <neilmarkoliverryan@gmail.com>
2016-03-28 19:12:33 +0000

    nitrous-backup at Mon Mar 28 19:12:33 UTC 2016

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+count = 1
+sum = 0
+  while count < limit
+    if count % 3 == 0 || count % 5 == 0
+      puts count
+      sum += count
+    end
+  count+=1
+  end
+  puts sum
+end
+
